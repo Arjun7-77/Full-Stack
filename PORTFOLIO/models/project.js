e85f6c8e87acf388a342c67fc9988382fc67ad1a
Arjun K S <arjunksc43@gmail.com>
2025-05-16 12:33:11 +0530

    Delete PORTFOLIO directory

--- PORTFOLIO/models/project.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const ProjectSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Project', ProjectSchema);
